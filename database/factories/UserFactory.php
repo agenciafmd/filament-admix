4a933f26f3f24dc33bf6bb7fa40eded427e71e93
Irineu Junior <irineu@fmd.ag>
2026-01-07 17:28:39 -0300

    refactor(files): lint

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,12 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Agenciafmd\Admix\Database\Factories;
 
 use Agenciafmd\Admix\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 
-class UserFactory extends Factory
+final class UserFactory extends Factory
 {
     protected $model = User::class;
 
