496fc984598521a48d8425550a364dfaa7e1baa2
Irineu Junior <irineu@fmd.ag>
2026-08-14 20:52:46 -0300

    chore(pint): aplica o pint nos blades

--- resources/views/filament/tables/columns/rating.blade.php
+++ resources/views/filament/tables/columns/rating.blade.php
@@ -3,18 +3,16 @@
 @endphp
 
 <div class="flex">
-    @for($i = 1; $i <= 5; $i++)
-        <div
-                @class([
-                    // TODO
-                    'text-gray-200' => $state < $i,
-                    'text-blue-500' => $state >= $i,
-                ])
-        >
+    @for ($i = 1; $i <= 5; $i++)
+        <div @class([
+            // TODO
+            'text-gray-200' => $state < $i,
+            'text-blue-500' => $state >= $i,
+        ])>
             <x-filament::icon
-                    color="{{ $state < $i ? 'gray' : 'orange' }}"
-                    icon="heroicon-s-star"
-                    class="w-6 h-6 pointer-events-none"
+                color="{{ $state < $i ? 'gray' : 'orange' }}"
+                icon="heroicon-s-star"
+                class="pointer-events-none h-6 w-6"
             />
         </div>
     @endfor
