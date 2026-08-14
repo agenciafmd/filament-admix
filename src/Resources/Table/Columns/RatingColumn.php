41d38c1910f9e2c679d0a0e865be83458ab6ec92
Irineu Junior <irineu@fmd.ag>
2026-08-14 20:43:58 -0300

    chore(composer): lint

--- src/Resources/Table/Columns/RatingColumn.php
+++ src/Resources/Table/Columns/RatingColumn.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Agenciafmd\Admix\Resources\Table\Columns;
 
 use Filament\Tables\Columns\Column;
 
-class RatingColumn extends Column
+final class RatingColumn extends Column
 {
     protected string $view = 'filament-admix::filament.tables.columns.rating';
 }
