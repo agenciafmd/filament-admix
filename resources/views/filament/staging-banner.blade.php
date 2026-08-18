1a52044e2cd14b22a78f9da7b369df0516e37e74
João Paulo dos Santos Ferreira <joao.paulo@fmd.ag>
2026-08-18 11:00:15 -0300

    chore(public): aplica estilo ao staging-banner.blade.php

--- resources/views/filament/staging-banner.blade.php
+++ resources/views/filament/staging-banner.blade.php
@@ -1,3 +1,3 @@
-<div class="flex items-center justify-center">
-    <p>{{ __('This is a staging environment and the information entered will not be reflected in the production environment.') }}</p>
+<div class="flex items-center justify-center p-2 bg-orange-500 text-white">
+    <p>{!! __('This is a staging environment and the information entered will not be reflected in the production environment.') !!}</p>
 </div>
